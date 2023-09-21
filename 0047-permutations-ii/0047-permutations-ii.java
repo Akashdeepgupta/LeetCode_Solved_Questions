ba1606dc96bf913cdd7f350bd8681c54f7d98aa8
Akashdeep Gupta <56160829+Akashdeepgupta@users.noreply.github.com>
2023-09-21 11:17:31 +0530

    Time: 573 ms (5.88%), Space: 44.5 MB (23.88%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+         List<List<Integer>> sol = new ArrayList<>();
+        List<Integer> unpro =new ArrayList<>();
+        List<Integer> pro =new ArrayList<>();
+        for(int num : nums){
+            unpro.add(num);
+        }
+        permutationArr(pro,unpro,0,0,sol);
+        return sol;
+    }
+
+    public  void permutationArr(List<Integer> pro,  List<Integer> unpro, int idxu, int idxp, List<List<Integer>> sol){
+        if(idxu == unpro.size()){
+            List<Integer> add = new ArrayList<>(pro);
+            if(!sol.contains(add)){
+                sol.add(add);
+            }
+            return;
+        }
+        int toAdd = unpro.get(idxu);
+        for (int i = 0; i <= pro.size(); i++) {
+            pro.add(i,toAdd);
+            permutationArr(pro,unpro,idxu+1,idxp,sol);
+            pro.remove(i);
+        }
+
+    }
+}

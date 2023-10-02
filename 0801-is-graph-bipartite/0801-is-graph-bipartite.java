c2108622618b6773a7b7d474839c31c963beee50
Akashdeep Gupta <56160829+Akashdeepgupta@users.noreply.github.com>
2023-10-02 13:52:24 +0530

    Time: 1 ms (65.96%), Space: 43.3 MB (97.99%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,16 +1,6 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        //create Adjacency list
-        ArrayList<ArrayList<Integer>> AdjList = new ArrayList<>();
         int n = graph.length;
-        for(int i = 0;i<n;i++){
-            ArrayList<Integer> al = new ArrayList<>();
-            AdjList.add(al);
-
-            for(int j = 0;j<graph[i].length;j++){
-                AdjList.get(i).add(graph[i][j]);
-            }
-        }
         int [] color = new int[n];
         for(int i = 0;i<color.length;i++){
             color[i] = -1;
@@ -18,7 +8,7 @@ class Solution {
 
         for(int i = 0;i<n;i++){
             if(color[i] == -1){
-                if(check(i,AdjList,color) == false){
+                if(check(i,graph,color) == false){
                     return false;
                 }
             }
@@ -26,14 +16,14 @@ class Solution {
         return true;
     }
 
-    public boolean check(int src, ArrayList<ArrayList<Integer>> graph , int [] color){
+    public boolean check(int src, int[][] graph , int [] color){
         color[src] = 0;
         Queue<Integer> q = new LinkedList<>();
         q.add(src);
         while(!q.isEmpty()){
             int node = q.peek();
             q.poll();
-            for(int nbr : graph.get(node)){
+            for(int nbr : graph[node] ){
                 if(color[nbr] == -1){
                     color[nbr] = 1-color[node];
                     q.add(nbr);

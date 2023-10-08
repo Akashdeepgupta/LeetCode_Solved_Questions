6522c13b2816a99cab359662d70b04fc3147447f
Akashdeep Gupta <56160829+Akashdeepgupta@users.noreply.github.com>
2023-10-08 21:57:15 +0530

    Time: 881 ms (5.56%), Space: 45.9 MB (6.94%) - LeetHub

---
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        // DP Question
+        int n = nums1.length;
+        int m = nums2.length;
+        int [][] dp = new int[m][n];
+        dp[0][0] = nums1[0]*nums2[0];
+
+        //fill 1st column
+        for(int i = 1;i<dp.length;i++){
+            dp[i][0] = Math.max(dp[i-1][0],nums2[i]*nums1[0]);
+        }
+
+        //fill 1st row
+        for(int j = 1;j<dp[0].length;j++){
+            dp[0][j] = Math.max(dp[0][j-1],nums1[j]*nums2[0]);
+        }
+
+        //fill remaining matrix
+        for(int i = 1;i<dp.length;i++){
+            for(int j = 1;j<dp[0].length;j++){
+                int top = dp[i-1][j];
+                int left = dp[i][j-1];
+                int currentMultiplication = nums1[j]*nums2[i];
+                int diagonal = dp[i-1][j-1];
+                int diagonalPlusCurrent = diagonal+currentMultiplication;
+                // dp[i][j] = Math.max(top,left,currentMultiplication,diagonal,diagonalPlusCurrent);
+                dp[i][j] = Math.max( top, Math.max(left, Math.max(currentMultiplication,Math.max(diagonal,diagonalPlusCurrent ) ) ) );
+            }
+        }
+        for(int i = 0;i<dp.length;i++){
+            for(int j = 0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        return dp[m-1][n-1];
+    }
+}

f6f4b5b3990eb4e96fe24766b92b2419e94ae99e
Akashdeep Gupta <56160829+Akashdeepgupta@users.noreply.github.com>
2024-03-12 21:16:49 +0530

    Time: 3 ms (54.35%) | Memory: 43.9 MB (68.97%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -8,21 +8,39 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ //approach 2 
+//  using map, linear approach
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
-        if(head == null){
-            return head;
-        }
-        ListNode temp = head;
-        int sum = 0;
-        while(temp!=null){
-            sum = sum+temp.val;
-            if(sum == 0){
-                return removeZeroSumSublists(temp.next);
+        HashMap<Integer,ListNode> hm = new HashMap<>();
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int prefixSum = 0;
+        hm.put(0,dummy);
+
+        while(head != null){
+            prefixSum = prefixSum + head.val;
+
+            if(hm.containsKey(prefixSum)){
+                ListNode start = hm.get(prefixSum);
+                ListNode temp = start;
+                int sum = prefixSum;
+                while(temp!=head){
+                    temp = temp.next;
+                    sum = sum + temp.val;
+                    if(temp!=head){
+                        hm.remove(sum);
+                    }
+                }
+                start.next = head.next;
             }
-            temp = temp.next;
+            else{
+                hm.put(prefixSum,head);
+            }
+
+            head = head.next;
         }
-        head.next = removeZeroSumSublists(head.next);
-        return head;
+
+        return dummy.next;
     }
 }

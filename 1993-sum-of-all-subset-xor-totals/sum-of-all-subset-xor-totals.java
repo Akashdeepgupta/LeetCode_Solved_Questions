43c51b2c03991e0bca87aefd0c617d572f039c45
Akashdeep Gupta <56160829+Akashdeepgupta@users.noreply.github.com>
2024-04-10 05:16:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (53.03%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,32 +1,15 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        ArrayList<Integer> al = new ArrayList<>();
-        int ans = helper(0,nums,al);
+        int ans = helper(0,nums,0);
         return ans;
     }
 
-    public int helper(int index, int [] nums,ArrayList<Integer> al){
+    public int helper(int index, int [] nums,int xor){
         if(index == nums.length){
-            int xorOfArray = xorOp(al);
-            return xorOfArray;
+            return xor;
         }
-        int ans = 0;
-        al.add(nums[index]);
-        ans = ans+helper(index+1,nums,al);
-        al.remove(al.size()-1);
-        ans = ans+helper(index+1,nums,al);
-        return ans;
-    }
-
-    public int xorOp(ArrayList<Integer> al){
-        int n = al.size();
-        if(n == 0){
-            return 0;
-        }
-        int xor = al.get(0);
-        for(int i =1;i<n;i++){
-            xor = xor^al.get(i);
-        }
-        return xor;
+        int withElement = helper(index+1,nums,xor^nums[index]);
+        int withoutElement = helper(index+1,nums,xor);
+        return withElement+withoutElement;
     }
 }

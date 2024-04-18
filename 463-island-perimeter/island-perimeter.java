7e14b394f67d6412aad38e2f3324458cf9fc0517
Akashdeep Gupta <56160829+Akashdeepgupta@users.noreply.github.com>
2024-04-18 23:38:26 +0530

    Time: 11 ms (9.41%) | Memory: 46.2 MB (5.11%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,40 +1,38 @@
 class Solution {
-    int count = 0;
     public int islandPerimeter(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         for(int i = 0;i<n;i++){
             for(int j= 0;j<m;j++){
                 if(grid[i][j] == 1){
-                    dfs(i,j,grid);
-                    return count;
+                    return dfs(i,j,grid);
                 }
             }
         }
         return -1;
     }
 
-    public void dfs(int i, int j,int[][] grid){
+    public int dfs(int i, int j,int[][] grid){
         int n = grid.length;
         int m = grid[0].length;
         int []xArr ={-1,0,+1,0};
         int []yArr ={0,+1,0,-1};
         if(i<0 || i>=n || j<0 || j>=m){
-            count++;
-            return;
+            return 1;
         }
         if(grid[i][j] == 0){
-            count++;
-            return;
+            return 1;
         }
         if(grid[i][j] == -1){
-            return;
+            return 0;
         }
         grid[i][j] = -1;
+        int ans = 0;
         for(int k = 0;k<4;k++){
             int xCordinate = i + xArr[k];
             int yCordinate = j + yArr[k];
-            dfs(xCordinate,yCordinate,grid);
+            ans = ans + dfs(xCordinate,yCordinate,grid);
         }
+        return ans;
     }
 }

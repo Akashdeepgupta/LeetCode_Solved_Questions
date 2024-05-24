6f529c6cc937f3e4234d896ba01ace399d0ffbba
Akashdeep Gupta <56160829+Akashdeepgupta@users.noreply.github.com>
2024-05-24 20:37:27 +0530

    Time: 753 ms (35.03%) | Memory: 44.9 MB (42.25%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -1,22 +1,38 @@
 class Solution {
+    int result = 0;
+    int K = 0;
     public int beautifulSubsets(int[] nums, int k) {
-        Arrays.sort(nums);
-        List<List<Integer>> res=new ArrayList<>();
-        f(0,nums,res,new ArrayList<>(),k);
-        return res.size();
+        K = k;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        solve(0,nums,hm);
+        return result-1; //excluding empty subset
     }
-    public void f(int ind,int[] nums,List<List<Integer>> res,List<Integer> ds,int k){
-        if(ind==nums.length){
-            if(ds.size()>0){
-                res.add(new ArrayList<>(ds));
-            }
+
+    public void solve(int idx,int [] nums, HashMap<Integer,Integer> hm){
+        if (idx == nums.length) {
+            result++;
             return;
         }
-        if(!(ds.contains(nums[ind]-k) )){
-            ds.add(nums[ind]);
-            f(ind+1,nums,res,ds,k);    
-            ds.remove(ds.size()-1);
+
+
+
+        //2 options take the idx element or do not take the idx element
+
+        //not take
+        solve(idx+1,nums,hm);
+
+        //take
+        //before taking check in hashmap if we can take it or not
+        if (!hm.containsKey(nums[idx] - K) && !hm.containsKey(nums[idx] + K)) {
+            hm.put(nums[idx], hm.getOrDefault(nums[idx], 0) + 1);
+            solve( idx + 1,nums, hm);
+            hm.put(nums[idx], hm.get(nums[idx]) - 1);
+
+            // Remove the key if its count drops to 0 to mimic the C++ erase behavior
+            if (hm.get(nums[idx]) == 0) {
+                hm.remove(nums[idx]);
+            }
         }
-        f(ind+1,nums,res,ds,k);
+
     }
 }
